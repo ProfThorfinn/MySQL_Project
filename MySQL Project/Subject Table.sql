3419f7ce0655825d54a96479bd43d7882aa56979
PROF <74806806+ProfThorfinn@users.noreply.github.com>
2025-01-11 22:59:01 +0200

    Update Subject Table.sql

--- MySQL Project/Subject Table.sql
+++ MySQL Project/Subject Table.sql
@@ -4,3 +4,13 @@ CREATE TABLE SUBJECTS (
 ID INT PRIMARY KEY,
 SUBJ_NAME VARCHAR(225)
 );
+
+-- SUBJECTS TABLE
+INSERT INTO SUBJECTS (ID, SUBJ_NAME)
+VALUES
+(1, "Physics"),
+(2, 'Chemistry'),
+(3, 'Biology'),
+(4, 'History'),
+(5, 'Geography'),
+(6, 'Philosophy');
