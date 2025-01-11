54af829086b08c7778952fbed5c0e93a25dfac06
PROF <74806806+ProfThorfinn@users.noreply.github.com>
2025-01-11 22:58:42 +0200

    Update Add Data to Subjects table.sql

--- MySQL Project/Add Data to Subjects table.sql
+++ MySQL Project/Add Data to Subjects table.sql
@@ -1,10 +1,1 @@
-USE EL_TMAIOZ_School;
--- SUBJECTS TABLE
-INSERT INTO SUBJECTS (ID, SUBJ_NAME)
-VALUES
-(1, "Physics"),
-(2, 'Chemistry'),
-(3, 'Biology'),
-(4, 'History'),
-(5, 'Geography'),
-(6, 'Philosophy');
+
